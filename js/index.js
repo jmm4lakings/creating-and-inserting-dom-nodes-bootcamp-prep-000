2fdd9aa3e3148c349e846b2785bbe69b13a00119
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-07 17:51:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,8 +3,11 @@ var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 
 element.style.backgroundColor = '#f9f9f9';
+
 document.body.appendChild(element);
+
 element.style.textAlign = 'center';
+
 var ul = document.createElement('ul');
  
 for (let i = 0; i < 3; i++) {
@@ -18,4 +21,5 @@ element.appendChild(ul);
 ul.style.textAlign = 'left';
 
 ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
 ul.remove();
